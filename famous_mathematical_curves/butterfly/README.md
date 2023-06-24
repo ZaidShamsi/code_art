44722317fba613b6bea5a167f064a0fe5a75f797
ZaidShamsi <zaidshamsi21@gmail.com>
2023-06-24 15:24:14 +0530

    butterfly curve corrected (mirrored along x then y)

--- famous_mathematical_curves/butterfly/README.md
+++ famous_mathematical_curves/butterfly/README.md
@@ -1,3 +1,15 @@
+# Butterfly
+
+Reads:
+- [Butterfly curve: algebraic (Wikipedia)](https://en.wikipedia.org/wiki/Butterfly_curve_(algebraic))
+
+## Equations
+- $y^{6}=x^{2}-x^{6}$
+
 # Output
 
 ![butterfly curve](butterfly.png)
+
+# Future work
+
+To plot [Butterfly curve: transcendental](https://en.wikipedia.org/wiki/Butterfly_curve_(transcendental)) using turtle module in Python.
--- famous_mathematical_curves/butterfly/butterfly.py
+++ famous_mathematical_curves/butterfly/butterfly.py
@@ -4,8 +4,12 @@ from matplotlib import pyplot as plt
 def butterfly():
     x = np.linspace(0, 1, 101)
     y = np.power((np.power(x, 2) - np.power(x, 6)), 1/6)
+    # mirror along x
     x = np.append(x, x)
     y = np.append(y, -y)
+    # mirror along y
+    x = np.append(-x, x)
+    y = np.append(y, y)
     return x, y
 
 # Butterfly
@@ -19,22 +23,20 @@ x, y = butterfly()
 # #59981A -> green
 # #81B622 -> lime green
 # #3D550C -> olive green
+# #ECF87F -> yellow green
 
-# fig = plt.figure(facecolor='#81B622')
-fig = plt.figure()
+fig = plt.figure(facecolor='#3D550C')
 ax = fig.add_subplot(111)
 
 ax.set_aspect('equal')
 ax.axis('off')
-ax.set_title('Butterfly', color='#3D550C', fontsize=16, fontname='Candara', fontweight='bold')
+ax.set_title('Butterfly', color='#ECF87F', fontsize=16, fontname='Candara', fontweight='bold')
 
 # once axis are turned off -> ax.axis('off) -> facecolor will also hide 
 # ax.set_facecolor("#005A9C")
 
-# swapping x and y to orient horizontally
-ax.plot(y, x, linestyle='-', linewidth=4, color='#A91B60')
-# using fill_betweenx instead of fill_between to fill between curve and y-axis
-ax.fill_betweenx(x, y, color='#FF0080')
+ax.plot(x, y, linestyle='-', linewidth=4, color='#A91B60')
+ax.fill_between(x, y, color='#FF0080')
 
 fig.savefig('butterfly.png')
 
