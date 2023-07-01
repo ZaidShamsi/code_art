8242361f8f2663df74b6f016b240813859c6c1fb
ZaidShamsi <zaidshamsi21@gmail.com>
2023-07-01 21:18:39 +0530

    added LogarithmicSpiral class and intertwine vector func, removed plot code

--- famous_mathematical_curves/logarithmic_spiral/logarithmic_spiral.py
+++ famous_mathematical_curves/logarithmic_spiral/logarithmic_spiral.py
@@ -1,29 +1,58 @@
 import numpy as np
-from matplotlib import pyplot as plt
 
 # Logarithmic spiral
-def logarithmic_spiral(k = 0.1):
-    phi = np.linspace(0, 6*np.pi, 721)
-    x = np.exp(k*phi)*np.cos(phi)
-    y = np.exp(k*phi)*np.sin(phi)
-    return x, y
+class LogarithmicSpiral():
+    def __init__(self, a, b, k, n, d):
+        # a -> controls the distance from the origin along the x-Axis
+        # b -> changes the starting value of the spiral along the x-Axis
+        # k -> controls the tightness of the successive rotations
+        # n -> number of spiral turns
+        # d -> number of points in which curve is discretized
+        self.a = a
+        self.b = b
+        self.k = k
+        self.n = n
+        self.d = d
+        
+    def xy_logarithmic_spiral(self):
+        phi = np.linspace(0, self.n*np.pi, self.n*self.d)
+        r = self.a*np.exp(self.k*phi)+self.b
+        x = r*np.cos(phi)
+        y = r*np.sin(phi)
+        return x, y
+    
+    def rotation(self, theta, orientation):
+        x, y = self.xy_logarithmic_spiral()
+        rotation_matrix = np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
+        xy_array = np.array([x, y])
+        if orientation == 'anti-clockwise':
+            rotated_square = np.dot(rotation_matrix, xy_array)
+            x = rotated_square[0, :]
+            y = rotated_square[1, :]
+        elif orientation == 'clockwise':
+            rotated_square = np.dot(np.transpose(xy_array), rotation_matrix)
+            x = rotated_square[:, 0]
+            y = rotated_square[:, 1]
+        return x, y
+    
+    def reflection(self, theta, orientation):
+        # reflection about a line at an angle
+        x, y = self.rotation(theta, orientation)
+        reflection_matrix = np.array([[np.cos(2*theta), np.sin(2*theta)], [np.sin(2*theta), -np.cos(2*theta)]])
+        xy_array = np.array([x, y])
+        if orientation == 'anti-clockwise':
+            reflected_spiral = np.dot(reflection_matrix, xy_array)
+            x = reflected_spiral[0, :]
+            y = reflected_spiral[1, :]
+        elif orientation == 'clockwise':
+            reflected_spiral = np.dot(np.transpose(xy_array), reflection_matrix)
+            x = reflected_spiral[:, 0]
+            y = reflected_spiral[:, 1]
+        return x, y
+    
 
-x, y = logarithmic_spiral()
-
-# shades of yellow
-# #FFEA00 -> bright yellow
-# #FFD700 -> gold
-
-fig = plt.figure(facecolor='black')
-ax = fig.add_subplot(111)
-
-ax.set_aspect('equal')
-# ax.axis('off') # I do not need turn off the axis as I have already made the figure background color black
-ax.set_title('Logarithmic spiral', color='#FFD700', fontsize=16, fontname='Candara')
-ax.set_facecolor("black")
-
-ax.plot(x, y, linestyle='-', linewidth=4, color='#FFD700')
-
-fig.savefig('logarithmic_spiral.png')
-
-plt.show()
+def intertwine_two_vectors(vector1, vector2):
+    intertwined_vector = [None]*(len(vector1)+len(vector2))
+    intertwined_vector[::2] = vector1
+    intertwined_vector[1::2] = vector2
+    return intertwined_vector
